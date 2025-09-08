cda44998692da9148b20431de84da23944c818b1
theonic100 <theo.nicholson.nz.100@gmail.com>
2025-09-08 21:57:41 +1200

    background and for loop
    background changes from yellow to black and trial of a for loop

--- music_cross.js
+++ music_cross.js
@@ -6,22 +6,37 @@ let center=640
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 //colorMode(HSB,50)
-background(255, 153, 0)
+//background(255, 153, 0)
 //stroke(15,2,245)
 // strokeWeight(200)
 //line(0,360,1280,360)
 // strokeWeight(70);
+fill(255,255,255)
+stroke(255,255,255)
+textSize(100)
+text(counter,100,200)
 
-let mountStroke = map(vocal,0,100,10,30)
-strokeWeight(mountStroke)
-stroke(mountStroke);
-fill(0, 255, 42);
+fill(0);
+noStroke();
+textSize(50);
+text(words,200,200);
+
+if(counter<900){background(0)}
+
+
+
+
+
+if(counter>900){background(255, 153, 0)}
+let mountStroke = map(vocal,0,100,10,30);
+strokeWeight(mountStroke);
+stroke(41, 89, 33);
+fill(34, 255, 0);
 triangle(370,720,880,720,639,240);//centre
 triangle(0,720,426,720,213,360);//left
 triangle(823,720,1280,720,1036,360);//right
- 
- stroke(255,0,0);
 
+ //cross info
 let offsetFromTop=50;
 let offsetFromBottom = 720 - 50;
 let halfWayDown = 720 / 2.5;
@@ -30,10 +45,33 @@ let leftx= center-(620/2);
 
 let crossStrokeWeight = map(drum, 0,100, 10,100);
 let crossOpacity = map(bass, 0, 100, 50, 255);
-stroke(255,0,0,crossOpacity)
+stroke(255,0,0);
 strokeWeight(crossStrokeWeight);
- line(center,offsetFromTop,center,offsetFromBottom);
- line(leftx,halfWayDown,leftx + 620, halfWayDown);
+ line(center,offsetFromTop,center,offsetFromBottom);//vert line
+line(leftx,halfWayDown,leftx + 620, halfWayDown);//horz line
+
+// if(counter<= 250)
+// for(let i = 0; i<2; i=i+1){
+//   stroke(255,0,0,crossOpacity)
+//   strokeWeight(crossStrokeWeight)
+
+// if (i === 0){
+//   //vert line
+// line(center,offsetFromTop,center,offsetFromBottom);
+// }else if (i === 1){
+//   //horz line
+// line(leftx,halfWayDown,leftx + 620, halfWayDown);
+// }
+// }
+
+// // store line coordinates in an array
+// let crossLines = [
+//   [center, offsetFromTop, center, offsetFromBottom],     // vertical
+//   [leftx, halfWayDown, leftx + 620, halfWayDown]         // horizontal
+// ];
+
+
+
 
 // let drumMap=map(vocal,0,100,300,680);
 //  let drumMap1=map(drum,0,100,180,540);
