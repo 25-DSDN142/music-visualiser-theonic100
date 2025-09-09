1cc0e1b51e5a6e82d22887e5f960b4d3aa3615ba
theonic100 <theo.nicholson.nz.100@gmail.com>
2025-09-09 15:12:57 +1200

    futher iteration
    9/09/2025

--- music_cross.js
+++ music_cross.js
@@ -2,51 +2,43 @@
 
 let center=640
 
+
 //vocal, drum, bass, and other are volumes ranging from 0 to 100
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+ 
 
 
 //background change
 if(counter<900){
-  //backOpacityA=map(counter,700,1000,255,0)
+  //backOpacityA=map(counter,850,1000,255,0)
     background(0,0,0)}
 
 if(counter>900){
-  background(255, 153, 0)}
+  //backOpacityB=map(counter,1000,1150,0,255)
+  background(3, 148, 252)}
 
-// fill(255)
-// noStroke()
-// textSize(100)
-// text(seconds,100,200)
-
+//counter
 fill(255)
 noStroke()
 textSize(100)
 text(counter,100,200)
 
-// fill(255);
-// noStroke();
-// textSize(50);
-// text(words,200,200);
 
-//circle sun
+//circle sun down 
 if(counter<900){
-circlesOpacity = map(counter,850,950,255,0)
+circlesOpacity = map(counter,850,950,255,0,true)
 fill(247, 195, 5,circlesOpacity)
 let circleY=map(counter,0,900,300,720,true)
 circle(640,circleY,400)
 }
-//stars
-// if(counter<900){
-//   let starsize=30
-// circlesOpacity = map(counter,850,950,255,0)
-// fill(255,255,255,circlesOpacity)
-// circle(100,100,starsize)
-// circle(30,50,starsize)
-// circle(700,50,starsize)
- //}
-
+//circle sun rise
+if(counter>900&& counter<1200){
+circlesOpacity = map(counter,950,1000,0,255,true)
+circleSize=map(bass,0,100,200,400)
+fill(247, 195, 5,circlesOpacity)
+circle(640,360,circleSize)
+}
 
  //cross variables
 let offsetFromTop=50;
@@ -56,115 +48,68 @@ let leftx= center-(620/2);
 let crossOpacity = 0; //cross fade in from 0
 
 //cross fade in and pulse
-if(counter>900){
-  crossOpacity = map(counter,900,1000,0,255,true);
+if(counter>1200 && counter<7200){
+  crossOpacity = map(counter,1200,1300,0,255,true);
+  crossOpacity = map(counter,7000,7200,255,0,true)
 let crossStrokeWeight = map(drum, 0,100, 10,100);
-//let crossOpacity = map(bass, 0, 100, 50, 255);
 stroke(255,0,0,crossOpacity);
 strokeWeight(crossStrokeWeight);
  line(center,offsetFromTop,center,offsetFromBottom);//vert line
 line(leftx,halfWayDown,leftx + 620, halfWayDown);//horz line
 }
 
-//2400 mark
+
+//2400 mark lines jump
 if(counter>2400 && counter < 3150){
+  lineOpacity=map(counter,2400,2500,0,255,true)
+lineOpacity=map(counter,3000,3150,255,0,true)
 lineLength=map(bass,0,100,320,160)
 lineLength1=map(drum,0,100,320,160)
 lineLength2=map(other,0,100,320,160)
-//lineOpacity=map(bass,2400,2600,0,255)
-stroke(255, 255, 255)
+stroke(255, 0,0,lineOpacity)
 strokeWeight(20)
-line(150,720,150,lineLength)
-line(200,720,200,lineLength1)
-line(250,720,250,lineLength2)
-line(1130,720,1130,lineLength)
-line(1080,720,1080,lineLength1)
-line(1030,720,1030,lineLength2)
+line(150,720,150,lineLength)//ll1
+line(200,720,200,lineLength1)//ll2
+line(250,720,250,lineLength2)//ll3
+line(1130,720,1130,lineLength)//rl3
+line(1080,720,1080,lineLength1)//rl2
+line(1030,720,1030,lineLength2)//rl3
 }
 
-if(counter>=7200){
-circleSize=map(drum,0,100,500,800)
-circle(640,360,circleSize)}
+//again and again
+if(counter>3150){
+//lineOpacity=map(counter,2400,2500,0,255,true)
+//lineOpacity=map(counter,3000,3150,255,0,true)
+vocalLength=map(vocal,0,100,200,300)
+stroke(255, 0,0)
+strokeWeight(20)
+line(150,0,150,vocalLength)//ll1
+line(200,0,200,vocalLength)//ll2
+line(250,0,250,vocalLength)//ll3
+line(1130,0,1130,vocalLength)//rl3
+line(1080,0,1080,vocalLength)//rl2
+line(1030,0,1030,vocalLength)//rl3
+}
+// //big drums circles
+// if(counter>=7200){
+// circleSize=map(drum,0,100,500,800)
+// circle(640,360,circleSize)}
 
+if (counter >= 7200) {
+  let numLayers = 5; // number of concentric circles
+  let baseSize = map(drum, 0, 100, 400, 600); // base size reacts to drum
+  let sizeStep = 50; // how much each layer increases
+  let alphaStep = 50; // opacity decrease for outer layers
 
+  noStroke();
 
-
-
-
-
-
-
-
-
-
-//three peaks
-// let mountStroke = map(vocal,0,100,10,30);
-// strokeWeight(mountStroke);
-// stroke(41, 89, 33);
-// fill(34, 255, 0);
-// triangle(370,720,880,720,639,240);//centre
-// triangle(0,720,426,720,213,360);//left
-// triangle(823,720,1280,720,1036,360);//right
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let crossOpacity = 0;
-// if(counter>=900)
-//   crossOpacity = map(counter,900,1000,0,255,true);
-// for(let i = 0; i<2; i=i+1){
-//   stroke(255,0,0,crossOpacity)
-//   strokeWeight(crossStrokeWeight)
-
-// if (i === 0){
-//   //vert line
-// line(center,offsetFromTop,center,offsetFromBottom);
-// }else if (i === 1){
-//   //horz line
-// line(leftx,halfWayDown,leftx + 620, halfWayDown);
-// }
-// }
-
-// // store line coordinates in an array
-// let crossLines = [
-//   [center, offsetFromTop, center, offsetFromBottom],     // vertical
-//   [leftx, halfWayDown, leftx + 620, halfWayDown]         // horizontal
-// ];
-
-
-
-
-// let drumMap=map(vocal,0,100,300,680);
-//  let drumMap1=map(drum,0,100,180,540);
-//  let drumMap2=map(drum,0,100,200,200);
-//  let drumMap3=map(drum,0,100,200,650); 
-
-
- //for(let i=1;i<drumMap;i=i+1){
-//var lineStep=i+10;
-//let center=i+10;
-// line(drumMap,drumMap2,drumMap,drumMap3);//vert top
-//     line(center,drumMap1,drumMap2,drumMap1);// horizontal left
-
- //  line(center1,liney2,center1+640,liney2);// horizontal left
- 
+  for (let i = 0; i < numLayers; i++) {
+    let circleSize = baseSize + i * sizeStep; // bigger for outer layers
+    let alpha = 255 - i * alphaStep; // fade out outer layers
+    fill(255, 150, 0, alpha); // orange with varying transparency
+    circle(640, 360, circleSize);
+  }
+}
 
 
 
