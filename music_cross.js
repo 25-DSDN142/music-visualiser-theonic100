7e42ed025f9c85845d82b5b7c667aba6b07fcf81
theonic100 <theo.nicholson.nz.100@gmail.com>
2025-09-09 12:15:52 +1200

    tutorial 9/09/2025
    whats has been done:
    - a sun was put on the first scene to be worked on.
    - Maps have been put in to other apsects of the code.
    - other code has been placed in to help with visualisation
    - a circle was placed in to be connected to the base on the song

--- music_cross.js
+++ music_cross.js
@@ -5,36 +5,48 @@ let center=640
 //vocal, drum, bass, and other are volumes ranging from 0 to 100
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-//colorMode(HSB,50)
-//background(255, 153, 0)
-//stroke(15,2,245)
-// strokeWeight(200)
-//line(0,360,1280,360)
-// strokeWeight(70);
+
+
 //background change
-if(counter<900){background(0)}
-if(counter>900){background(255, 153, 0)}
+if(counter<900){
+  //backOpacityA=map(counter,700,1000,255,0)
+    background(0,0,0)}
+
+if(counter>900){
+  background(255, 153, 0)}
+
+// fill(255)
+// noStroke()
+// textSize(100)
+// text(seconds,100,200)
 
 fill(255)
 noStroke()
 textSize(100)
 text(counter,100,200)
 
-fill(255);
-noStroke();
-textSize(50);
-text(words,200,200);
+// fill(255);
+// noStroke();
+// textSize(50);
+// text(words,200,200);
 
+//circle sun
+if(counter<900){
+circlesOpacity = map(counter,850,950,255,0)
+fill(247, 195, 5,circlesOpacity)
+let circleY=map(counter,0,900,300,720,true)
+circle(640,circleY,400)
+}
+//stars
+// if(counter<900){
+//   let starsize=30
+// circlesOpacity = map(counter,850,950,255,0)
+// fill(255,255,255,circlesOpacity)
+// circle(100,100,starsize)
+// circle(30,50,starsize)
+// circle(700,50,starsize)
+ //}
 
-
-//three peaks
-let mountStroke = map(vocal,0,100,10,30);
-strokeWeight(mountStroke);
-stroke(41, 89, 33);
-fill(34, 255, 0);
-triangle(370,720,880,720,639,240);//centre
-triangle(0,720,426,720,213,360);//left
-triangle(823,720,1280,720,1036,360);//right
 
  //cross variables
 let offsetFromTop=50;
@@ -44,7 +56,7 @@ let leftx= center-(620/2);
 let crossOpacity = 0; //cross fade in from 0
 
 //cross fade in and pulse
-if(counter>=900){
+if(counter>900){
   crossOpacity = map(counter,900,1000,0,255,true);
 let crossStrokeWeight = map(drum, 0,100, 10,100);
 //let crossOpacity = map(bass, 0, 100, 50, 255);
@@ -53,6 +65,65 @@ strokeWeight(crossStrokeWeight);
  line(center,offsetFromTop,center,offsetFromBottom);//vert line
 line(leftx,halfWayDown,leftx + 620, halfWayDown);//horz line
 }
+
+//2400 mark
+if(counter>2400 && counter < 3150){
+lineLength=map(bass,0,100,320,160)
+lineLength1=map(drum,0,100,320,160)
+lineLength2=map(other,0,100,320,160)
+//lineOpacity=map(bass,2400,2600,0,255)
+stroke(255, 255, 255)
+strokeWeight(20)
+line(150,720,150,lineLength)
+line(200,720,200,lineLength1)
+line(250,720,250,lineLength2)
+line(1130,720,1130,lineLength)
+line(1080,720,1080,lineLength1)
+line(1030,720,1030,lineLength2)
+}
+
+if(counter>=7200){
+circleSize=map(drum,0,100,500,800)
+circle(640,360,circleSize)}
+
+
+
+
+
+
+
+
+
+
+
+
+//three peaks
+// let mountStroke = map(vocal,0,100,10,30);
+// strokeWeight(mountStroke);
+// stroke(41, 89, 33);
+// fill(34, 255, 0);
+// triangle(370,720,880,720,639,240);//centre
+// triangle(0,720,426,720,213,360);//left
+// triangle(823,720,1280,720,1036,360);//right
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // let crossOpacity = 0;
